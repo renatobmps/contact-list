277a23ef28a74b90503f0843a40caf850f549fba
Renato Brandão <renatobmpsilva@gmail.com>
2022-10-15 16:49:10 -0300

    test

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.use(session({
-  secret: process.env.SECRET, cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false
+  secret: process.env.SECRET,
+  cookie: { maxAge: 60000 },
+  resave: false,
+  saveUninitialized: false
 }));
 
 app.use(express.json());
